da61fb536f5f808333fa6ae69fc932e7bc536d18
manojsingh <manojsingh.manoj@gmail.com>
2026-08-29 18:13:20 +0200

    chore: ignore local observability data

--- alloy/grafana-data/plugins/grafana-metricsdrilldown-app/6709.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgrafana_metricsdrilldown_app=self.webpackChunkgrafana_metricsdrilldown_app||[]).push([["6709"],{180(a,n,i){i.d(n,{initPluginTranslations:()=>r.wR});var r=i(8952)}}]);
